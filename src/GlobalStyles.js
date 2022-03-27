cdd960fa4857b921ef313ff7c066516b1bf8193d
Isaac Owomugisha <archimedesisaac@gmail.com>
2022-03-27 13:34:49 +0300

    Setup tailwindcss

---
+++ src/GlobalStyles.js
@@ -0,0 +1,15 @@
+import tw, {styled} from 'twin.macro';
+
+export const Wrapper = styled.div`
+  ${tw`
+    grid
+    grid-cols-1
+    md:grid-cols-2
+    mx-5
+    my-3
+    md:mx-16
+    md:my-5
+    lg:mx-20
+    lg:my-10
+   `}
+`;
