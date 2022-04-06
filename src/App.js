500b313fd4b7949ad32de6af39f09bfc027d8916
Lydia Sanyu Naggayi <37118361+NLSanyu@users.noreply.github.com>
2022-04-06 13:51:57 +0300

    Initialize GA tracking

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import ReactGA from 'react-ga';
 import './App.css';
 import {Wrapper} from "./GlobalStyles";
 import Header from "./components/Header";
 import Translate from "./components/Translate";
+
+const GA_TRACKING_ID = process.env.TRACKING_ID;
+ReactGA.initialize(GA_TRACKING_ID);
+ReactGA.pageview(window.location.pathname);
 
 function App() {
     return (
