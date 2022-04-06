5c4f4452abd2a271954d35286f133746cda2fe0d
Lydia Sanyu Naggayi <37118361+NLSanyu@users.noreply.github.com>
2022-04-06 15:02:21 +0300

    Upgrade to GA4, replacing react-ga with react-ga4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import ReactGA from 'react-ga';
-import './App.css';
+import ReactGA from "react-ga4";
+import "./App.css";
 import {Wrapper} from "./GlobalStyles";
 import Header from "./components/Header";
 import Translate from "./components/Translate";
+import {useEffect} from "react";
 
-const GA_TRACKING_ID = process.env.TRACKING_ID;
-ReactGA.initialize(GA_TRACKING_ID);
-ReactGA.pageview(window.location.pathname);
+const GA4_TRACKING_ID = process.env.GA4_TRACKING_ID;
 
 function App() {
+    useEffect(() => {
+        ReactGA.initialize(GA4_TRACKING_ID);
+        ReactGA.send("pageview");
+    }, []);
     return (
         <div className="h-screen">
             <Header/>
