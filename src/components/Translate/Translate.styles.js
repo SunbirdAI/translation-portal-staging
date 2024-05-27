528e655157bb7b7a1c565c81ab73da64bd499a5b
gilbertyiga <vigidaiz15@gmail.com>
2024-05-27 12:24:17 +0300

    Added a removable note and filters

--- src/components/Translate/Translate.styles.js
+++ src/components/Translate/Translate.styles.js
@@ -1,4 +1,4 @@
-import tw, {styled} from "twin.macro";
+import tw, { styled } from "twin.macro";
 
 export const MainContainer = styled.div`
   ${tw`
@@ -7,5 +7,33 @@ export const MainContainer = styled.div`
     md:grid-cols-2
     rounded-md
     m-3
-   `}
+  `}
+`;
+
+export const Note = styled.div`
+  ${tw`
+    flex
+    w-full
+    items-center
+    p-6
+    bg-yellow-200
+    text-yellow-800
+    rounded-md
+    mb-4
+    justify-between
+    relative
+  `}
+`;
+
+export const CloseButton = styled.button`
+  ${tw`
+    absolute
+    top-2
+    right-2
+    bg-transparent
+    border-none
+    text-yellow-800
+    cursor-pointer
+    text-lg
+  `}
 `;
--- src/components/Translate/index.js
+++ src/components/Translate/index.js
@@ -1,4 +1,4 @@
-import { MainContainer } from "./Translate.styles";
+import { MainContainer, Note, CloseButton } from "./Translate.styles";
 import TranslateTextArea from "../TranslateTextArea";
 import SamplePhrases from "../SamplePhrases";
 import { useEffect, useRef, useState, useCallback } from "react";
@@ -16,15 +16,21 @@ const localLangOptions = [
 
 const autoOption = [{ label: 'Auto detection', value: 'auto-detection' }];
 const sourceOptions = [...autoOption, ...localLangOptions];
-const getTargetOptions = () => localLangOptions;
+// const getTargetOptions = () => localLangOptions;
+
+const getTargetOptions = (sourceLanguage) => {
+    // Filter out the selected source language from all available options
+    return localLangOptions.filter(option => option.value !== sourceLanguage);
+};
 
 const Translate = () => {
     const [sourceLanguage, setSourceLanguage] = useState('eng');
-    const [targetLanguage, setTargetLanguage] = useState(localLangOptions[0].value);
+    const [targetLanguage, setTargetLanguage] = useState(localLangOptions[1].value);
     const [sourceText, setSourceText] = useState('');
     const [translation, setTranslation] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [detectedLanguage, setDetectedLanguage] = useState('');
+    const [showNote, setShowNote] = useState(true);
     const prevTarget = useRef();
     const isMounted = useRef(false);
     const isComponentMounted = useRef(true);
@@ -55,10 +61,9 @@ const Translate = () => {
         try {
             const detectedLanguage = await languageId(text);
             if (isComponentMounted.current) {
-                if (detectedLanguage !== targetLanguage) {
-                    setDetectedLanguage(detectedLanguage);
-                    setSourceLanguage(detectedLanguage);
-                } else {
+                setDetectedLanguage(detectedLanguage);
+                setSourceLanguage(detectedLanguage);
+                if (detectedLanguage === targetLanguage) {
                     setTranslation("Detected language is the same as the target language.")
                     console.error("Detected language is the same as the target language.");
                 }
@@ -108,30 +113,39 @@ const Translate = () => {
     }, [sourceText, targetLanguage, sourceLanguage, translate, translation]);
 
     return (
-        <MainContainer>
-            <TranslateTextArea
-                placeholder="Enter text"
-                dropDownOptions={sourceOptions}
-                setSourceLanguage={setSourceLanguage}
-                text={sourceText}
-                setText={setSourceText}
-                detectedLanguage={detectedLanguage}
-            />
-            <TranslateTextArea
-                placeholder="Translation"
-                disabled={true}
-                dropDownOptions={getTargetOptions()}
-                setTargetLanguage={setTargetLanguage}
-                translation={translation}
-                text={sourceText}
-                sourceLanguage={sourceLanguage}
-                targetLanguage={targetLanguage}
-                isLoading={isLoading}
-                handleTextToSpeech={handleTextToSpeech}
-                showCopyButton={true}
-            />
-            <SamplePhrases sourceLanguage={sourceLanguage} setSamplePhrase={setSourceText} />
-        </MainContainer>
+        <div>
+            {showNote && (
+                <Note>
+                    <strong>Note:</strong> Our auto language detection currently supports the following languages: English, Luganda, Acholi, Ateso, Lugbara, and Runyankole.
+                    We are actively working on improving this feature and adding support for more languages in the future.
+                    <CloseButton onClick={() => setShowNote(false)}>✖</CloseButton>
+                </Note>
+            )}
+            <MainContainer>
+                <TranslateTextArea
+                    placeholder="Enter text"
+                    dropDownOptions={sourceOptions}
+                    setSourceLanguage={setSourceLanguage}
+                    text={sourceText}
+                    setText={setSourceText}
+                    detectedLanguage={detectedLanguage}
+                />
+                <TranslateTextArea
+                    placeholder="Translation"
+                    disabled={true}
+                    dropDownOptions={getTargetOptions(sourceLanguage)}
+                    setTargetLanguage={setTargetLanguage}
+                    translation={translation}
+                    text={sourceText}
+                    sourceLanguage={sourceLanguage}
+                    targetLanguage={targetLanguage}
+                    isLoading={isLoading}
+                    handleTextToSpeech={handleTextToSpeech}
+                    showCopyButton={true}
+                />
+                <SamplePhrases sourceLanguage={sourceLanguage} setSamplePhrase={setSourceText} />
+            </MainContainer>
+        </div>
     );
 };
 
