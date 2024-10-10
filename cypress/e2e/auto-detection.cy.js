f2d0127ae401c2f75d358b24f0ef3f5384ca6152
evie-8 <neouma29@gmail.com>
2024-10-10 15:08:59 +0300

    adding a notification when user enters a text longer than 5000 characters

--- cypress/e2e/auto-detection.cy.js
+++ cypress/e2e/auto-detection.cy.js
@@ -12,7 +12,14 @@ describe("Auto Detection of Language", () => {
         // Step 1: Enter text into the textarea
         cy.get('textarea[placeholder="Enter text"]').clear().type(text);
 
-        cy.wait(3000);
+        cy.wait(6000);
+        if (expectedLanguage === "language not detected") {
+          cy.get('[data-testid="feedback-button"]').should("not.exist");
+          cy.get('[data-testid="copy-container"]').should("not.exist");
+        } else {
+          cy.get('[data-testid="feedback-button"]').should("exist");
+          cy.get('[data-testid="copy-container"]').should("exist");
+        }
         // Step 2: Verify the detected language
         cy.get('[data-testid="detected-language"]')
           .invoke("text")
