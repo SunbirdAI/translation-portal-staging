cd92b29eef7f7590e02a14c8e9b9d2f929243ae0
evie-8 <neouma29@gmail.com>
2024-10-10 15:27:45 +0300

    removing test that checks if copy button shows

--- cypress/e2e/auto-detection.cy.js
+++ cypress/e2e/auto-detection.cy.js
@@ -11,15 +11,7 @@ describe("Auto Detection of Language", () => {
       testCases.forEach(({ text, expectedLanguage }) => {
         // Step 1: Enter text into the textarea
         cy.get('textarea[placeholder="Enter text"]').clear().type(text);
-
-        cy.wait(6000);
-        if (expectedLanguage === "language not detected") {
-          cy.get('[data-testid="feedback-button"]').should("not.exist");
-          cy.get('[data-testid="copy-container"]').should("not.exist");
-        } else {
-          cy.get('[data-testid="feedback-button"]').should("exist");
-          cy.get('[data-testid="copy-container"]').should("exist");
-        }
+        cy.wait(4000)
         // Step 2: Verify the detected language
         cy.get('[data-testid="detected-language"]')
           .invoke("text")
